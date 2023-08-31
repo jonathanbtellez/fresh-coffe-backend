2ffe4de204bef3592ffea6b5f06d7b5451c4a859
jonathanbtellez <jonathanbtellez@gmail.com>
2023-08-30 20:29:32 -0500

    Create model order and its migration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +24,8 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/orders', Order::class );
+
 });
 
 Route::apiResource('/categories', CategoryController::class);
